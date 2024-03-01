f59fd6ae81d4f118da1edd255ca268926a7d9cfd
entermene <Yhmecnu@163.com>
2024-03-01 18:04:15 +0800

    debug

--- backtest/strategy.py
+++ backtest/strategy.py
@@ -24,27 +24,25 @@ class tradeSignal:
     HARD_SELL: float = -0.5
 
 
-# def make_seqs(seq_len: int, data: torch.Tensor):
-#     num_samp = data.size(0)
-#     stack = []
-#     for i in range(0, num_samp, seq_len):
-#         stack.append(data[i : min(i + seq_len, num_samp)])
-#     add = torch.zeros_like(stack[0])
-#     last_seq = stack[-1].size(0)
-#     if last_seq < seq_len:
-#         add[:last_seq, :] = stack[-1]
-#         for i in range(last_seq, seq_len):
-#             add[i, :] = stack[-1][-1, :]
-#     stack[-1] = add
-#     return torch.stack(stack, dim=0)
-
-
 def read_data(code: str) -> pd.DataFrame:
     data_path = Path(__file__).parent.parent / f"data/{code}_test_data.csv"
     if os.path.exists(data_path):
         test_data = pd.read_csv(data_path)
     else:
         _, test_data = make_data(code)
+    return test_data
+
+
+def read_orin_data(code: str) -> pd.DataFrame:
+    file_path = Path(__file__).parent.parent / f"data/{code}.csv"
+    fu_dat = pd.read_csv(file_path)
+    features = fu_dat.drop(columns=["change1", "ts_code"])
+    data = features.iloc[:-1, :-1]
+    test_data = (
+        data[data["trade_date"] >= 20220913]
+        .drop(columns=["trade_date"])
+        .reset_index(drop=True)
+    )
     return test_data
 
 
@@ -55,17 +53,9 @@ def make_vgg_data(code: str, seq_len: int) -> torch.Tensor:
     return features
 
 
-def flat_data(data: pd.DataFrame, windows: int) -> pd.DataFrame:
-    test_data = []
-    num_samp = len(data)
-    for i in range(0, num_samp, windows):
-        test_data.append(data[i : min(i + windows, num_samp)].values.flatten())
-    return pd.DataFrame(test_data)
-
-
 def make_gbdt_data(code: str, seq_len: int):
     _, _, test_data, _ = split_data(code, seq_len, False)
-    return test_data.fillna(method="ffill")
+    return test_data.ffill()
 
 
 def execut_signal(
@@ -95,7 +85,7 @@ def vgg_lstm_strategy(code: str, seq_len: int):
     has_siganl = False
     weight = torch.tensor([-0.5, -0.2, 0.0, 0.2, 0.5], dtype=torch.float32)
     account = futureAccount(current_date="2022-09-13")
-    data = read_data(code)
+    data = read_orin_data(code)
     test_data = make_vgg_data(code, seq_len)
     for i in range(len(data)):
         account.update_date(1)
@@ -121,9 +111,8 @@ def gbdt_strategy(code: str, seq_len: int):
     has_siganl = False
     weight = torch.tensor([-0.5, -0.2, 0.0, 0.2, 0.5], dtype=torch.float32)
     account = futureAccount(current_date="2022-09-13")
-    data = read_data(code)
+    data = read_orin_data(code)
     test_data = make_gbdt_data(code, seq_len)
-    print(test_data.shape)
     for i in range(len(data)):
         account.update_date(1)
         price = data.loc[i, ["close"]].item()
@@ -132,17 +121,43 @@ def gbdt_strategy(code: str, seq_len: int):
             has_siganl = False
         account.update_price({code: price})
         portfolio_values.append(account.portfolio_value)
-        if (i + 1) % seq_len == 0 or (i + 1) == len(data):
+        if (i + 1) >= seq_len and i <= len(data) - seq_len:
             signals = torch.tensor(
-                generate_signal(test_data[pre_times], model), dtype=torch.float32
+                generate_signal(test_data.iloc[pre_times, :], model),
+                dtype=torch.float32,
             )
             pre_times += 1
             has_siganl = True
     return portfolio_values
 
 
+def random_strategy(code: str, seq_len: int):
+    pre_times = 0
+    signals = None
+    portfolio_values = []
+    has_siganl = False
+    weight = torch.tensor([-0.5, -0.2, 0.0, 0.2, 0.5], dtype=torch.float32)
+    account = futureAccount(current_date="2022-09-13")
+    data = read_orin_data(code)
+    for i in range(len(data)):
+        account.update_date(1)
+        price = data.loc[i, ["close"]].item()
+        if has_siganl:
+            execut_signal(code, account, weight, signals, price)
+            has_siganl = False
+        account.update_price({code: price})
+        account.calculate_portfolio_value()
+        portfolio_values.append(account.portfolio_value)
+        if (i + 1) >= seq_len and i <= len(data) - seq_len:
+            signals = torch.randn(5)
+            pre_times += 1
+            has_siganl = True
+    return portfolio_values
+
+
 if __name__ == "__main__":
-    result = gbdt_strategy("IC.CFX", 50)
+    result = random_strategy("IC.CFX", 50)
+    # print(pd.DataFrame(result).iloc[:, 0].values)
     df = pd.DataFrame(result).iloc[:, 0] / pd.DataFrame(result).iloc[0, 0].item()
     df.dropna().plot()
     plt.show()
