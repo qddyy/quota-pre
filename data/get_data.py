58a885c1a457cf167396d13fdb143c32c2e2adcf
entermene <Yhmecnu@163.com>
2024-03-08 12:57:36 +0800

    add etf  data

--- data/get_data.py
+++ data/get_data.py
@@ -11,10 +11,10 @@ today = int(date.today().strftime("%Y%m%d"))
 fut_codes = ["IH.CFX", "IF.CFX", "IC.CFX", "IM.CFX"]
 index_codes = ["000016.SH", "000300.SH", "000905.SH", "000852.SH"]
 fut_info = {
-    "IH.CFX": ["000016.SH", 20160224, today],
-    "IF.CFX": ["000300.SH", 20160224, today],
-    "IC.CFX": ["000905.SH", 20160224, today],
-    "IM.CFX": ["000852.SH", 20221130, today],
+    "IH.CFX": ["000016.SH", "510050.SH", 20160224, today],
+    "IF.CFX": ["000300.SH", "159919.SZ", 20160224, today],
+    "IC.CFX": ["000905.SH", "159922.SZ", 20160224, today],
+    "IM.CFX": ["000852.SH", "159845.SZ", 20221130, today],
 }
 indics = [
     "ma",
@@ -33,8 +33,8 @@ indics = [
 
 
 def get_fu_data(code: str) -> pd.DataFrame:
-    start_date = fut_info[code][1]
-    end_date = fut_info[code][2]
+    start_date = fut_info[code][-2]
+    end_date = fut_info[code][-1]
     etf_fu = pro.fut_daily(ts_code=code, start_date=start_date, end_date=end_date)
     etf_fu = etf_fu[
         [
@@ -90,8 +90,8 @@ def save_fut_data(codes: list[str], indis: list[str] | None = None) -> None:
 
 def get_fu_index(fu_code: str) -> pd.DataFrame:
     code = fut_info[fu_code][0]
-    start_date = fut_info[fu_code][1]
-    end_date = fut_info[fu_code][2]
+    start_date = fut_info[fu_code][-2]
+    end_date = fut_info[fu_code][-1]
     ind_dat = pro.index_daily(
         ts_code=code, start_date=start_date, end_date=end_date
     ).drop(columns=["amount", "ts_code"])
@@ -105,7 +105,26 @@ def merge_ind(data: pd.DataFrame, fu_code: str) -> pd.DataFrame:
     return merge_dat
 
 
+def merge_etf(data: pd.DataFrame, fu_code: str):
+    ind = get_fu_etf(fu_code)
+    merge_dat = pd.merge(data, ind, on="trade_date")
+    return merge_dat
+
+
+def get_fu_etf(fu_code: str) -> pd.DataFrame:
+    code = fut_info[fu_code][1]
+    print(code)
+    start_date = fut_info[fu_code][-2]
+    end_date = fut_info[fu_code][-1]
+    print(end_date)
+    etf_dat = pro.fund_daily(
+        ts_code=code, start_date=start_date, end_date=end_date
+    ).drop(columns=["amount", "ts_code"])
+    etf_dat.columns = ["etf_" + v if v != "trade_date" else v for v in etf_dat.columns]
+    return etf_dat
+
+
 if __name__ == "__main__":
-    ind = get_fu_index(fut_codes[3])
+    etf = get_fu_etf(fut_codes[3])
     fu = get_fu_data(code=fut_codes[3])
-    print(pd.merge(fu, ind, on="trade_date"))
+    print(etf)
