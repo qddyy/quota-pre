b9d5c800a10ed8b845a3788e272b568cab4ac9a4
entermene <Yhmecnu@163.com>
2024-03-15 16:12:36 +0800

    add aggregate model

--- test_model.py
+++ test_model.py
@@ -14,8 +14,12 @@ input_dim = int(os.environ["INPUT_DIM"])
 batch_size = int(os.environ["BATCH_SIZE"])
 hidden_dim = int(os.environ["HIDDEN_DIM"])
 code = os.environ["CODE"]
+if_agg = bool(os.environ["IF_AGG"])
 
 model_path = Path(__file__).parent / f"vgg_lstm_model_{code}.pth"
+if if_agg:
+    model_path = Path(__file__).parent / "vgg_lstm_model_agg.pth"
+    print("---------------test agg model---------------")
 test_data = lstm_test_data(code, batch_size, seq_len)
 # criterion = torch.nn.MSELoss(reduction="sum")
 criterion = CustomLoss()
